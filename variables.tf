b7e439369d1214730fdeac5ab08af1ddd72faa56
Steven Black <genseb7@gmail.com>
2020-05-18 15:41:36 -0400

    sort vars

--- variables.tf
+++ variables.tf
@@ -5,63 +5,63 @@ variable "apply_immediately" {
 }
 
 variable "auto_minor_version_upgrade" {
+  default     = true
   description = "Specifies whether a minor engine upgrades will be applied automatically to the underlying Cache Cluster instances during the maintenance window."
   type        = bool
-  default     = true
 }
 
 variable "availability_zones" {
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
   description = "List of EC2 availability zones in which the replication group's cache clusters will be created"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "cluster_size" {
+  default     = 4
   description = "Number of nodes in Elasticache cluster"
   type        = number
-  default     = 4
 }
 
 variable "egress_security_group_rule_description" {
+  default     = null
   description = "Description for Egress rule"
   type        = string
-  default     = null
 }
 
 variable "engine_version" {
+  default     = "4.0.10"
   description = "Elasticache engine version"
   type        = string
-  default     = "4.0.10"
 }
 
 variable "ingress_cidr_sg_rule_description" {
+  default     = null
   description = "Description for CIDR-block Ingress rule"
   type        = string
-  default     = null
 }
 
 variable "ingress_rule_cidr_blocks" {
+  default     = []
   description = "List of CIDR blocks that are allowed ingress to ElastiCache"
   type        = list(string)
-  default     = []
 }
 
 variable "ingress_sg_sg_rule_description" {
+  default     = null
   description = "Description for Security Group Ingress rule"
   type        = string
-  default     = null
 }
 
 variable "instance_type" {
+  default     = "cache.t2.micro"
   description = "Type of ec2 instance for elasticache"
   type        = string
-  default     = "cache.t2.micro"
 }
 
 variable "maintenance_window" {
-  type        = string
   default     = "sun:03:00-sun:04:00"
   description = "Specifies the weekly time range for when maintenance on the cache cluster is performed. The format is ddd:hh24:mi-ddd:hh24:mi (24H Clock UTC). The minimum maintenance window is a 60 minute period. Example: sun:05:00-sun:09:00"
+  type        = string
 }
 
 variable "name" {
@@ -70,39 +70,39 @@ variable "name" {
 }
 
 variable "notification_topic_arn" {
-  type        = string
   default     = null
   description = "An Amazon Resource Name (ARN) of an SNS topic to send ElastiCache notifications to. Example: arn:aws:sns:us-east-1:012345678999:my_sns_topic"
+  type        = string
 }
 
 variable "parameter_group_name" {
+  default     = "default.redis4.0"
   description = "Elasticache parameter group name"
   type        = string
-  default     = "default.redis4.0"
 }
 
 variable "port" {
+  default     = 6379
   description = "Elasticache port"
   type        = number
-  default     = 6379
 }
 
 variable "replication_group_description" {
+  default     = null
   description = "Description for ElastiCache Replication Group"
   type        = string
-  default     = null
 }
 
 variable "security_group_ids" {
+  default     = []
   description = "Security Groups that are allowed ingress to ElastiCache"
   type        = set(string)
-  default     = []
 }
 
 variable "snapshot_window" {
+  default     = "02:00-03:00"
   description = "(Optional, Redis only) The daily time range (in UTC) during which ElastiCache will begin taking a daily snapshot of your cache cluster. The minimum snapshot window is a 60 minute period. Example: 05:00-09:00"
   type        = string
-  default     = "02:00-03:00"
 }
 
 variable "subnet_ids" {
@@ -111,9 +111,9 @@ variable "subnet_ids" {
 }
 
 variable "tags" {
+  default     = {}
   description = "User-Defined tags"
   type        = map(string)
-  default     = {}
 }
 
 variable "vpc_id" {
