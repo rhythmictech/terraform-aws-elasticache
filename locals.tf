f0b44ec56fc2025a4b470d74b631eaf946ce8c94
Steven Black <genseb7@gmail.com>
2020-06-01 10:22:46 -0400

    delete local.route53_record_name

--- locals.tf
+++ locals.tf
@@ -7,7 +7,6 @@ locals {
   ingress_sg_sg_rule_description         = var.ingress_sg_sg_rule_description
   name                                   = var.name
   replication_group_description          = coalesce(var.replication_group_description, "Replication group for ${local.name}, managed by terraform")
-  route53_record_name                    = local.create_route_53_cname_record ? "${local.name}-CNAME" : ""
   security_group_name                    = "${local.name}-SG"
   subnet_group_name                      = "${local.name}-NET"
   tags                                   = var.tags
