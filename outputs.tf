f4cc5e74d41f6376dddbd8afbcbbaa290ca23dcf
Steven Black <genseb7@gmail.com>
2020-06-01 10:30:52 -0400

    add fqdn

--- outputs.tf
+++ outputs.tf
@@ -15,3 +15,8 @@ output "elasticache_route53_record" {
   description = "aws route53 record resource"
   value       = try(aws_route53_record.elasticache[0], {})
 }
+
+output "elasticache_fqdn" {
+  description = "Fully Qualified Domain Name of Elasticache: the endpoint or the CNAME if used"
+  value       = try(aws_route53_record.elasticache[0].fqdn, aws_elasticache_replication_group.this.primary_endpoint_address)
+}
